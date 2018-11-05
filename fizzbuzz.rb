cf58941519eb31345a17679492fe9e03d849c02e
Janine Luk <janineluk95@gmail.com>
2018-11-05 16:32:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './fizzbuzz_spec.rb'
+
+def fizzbuzz(int)
+  if int % 3 == 0 
+    "Fizz"
+  end
+  
+  elsif int % 5 == 0 
+    "Buzz"
+  end
+  
+  elsif int % 15 == 0 
+    "FizzBuzz"
+  
+end
 
