7440b0532abe2328394743cd8a5fd8cd34dd70d4
Janine Luk <janineluk95@gmail.com>
2018-11-05 16:37:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     "Fizz"
-  elsif int % 5 == 0 
+  elsif int % 3 || int % 5 == 0 
     "Buzz"
   elsif int % 3 && int % 5 == 0 
     "FizzBuzz"
-  else int % 3 || int % 5w == 0 
+  else int % 4 == 0 
     nil
   end
 end
